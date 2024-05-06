84208737e59453f5337b258e68536da1d640fe32
Yavor Manev <yavor.manev.a54@learn.telerikacademy.com>
2024-05-06 17:54:22 +0300

    ChangeDoorName Added

--- Pages/Admin.cshtml.cs
+++ Pages/Admin.cshtml.cs
@@ -8,19 +8,22 @@ namespace GarageDoorsWeb.Pages
     public class AdminModel : PageModel
     {
         private readonly IUserService _userService;
-        
+        private readonly IDoorService _doorService;
+
         [BindProperty]
         public User NewUser { get; set; }
         public IEnumerable<User> Users { get; set; }
+        public List<Door> Doors { get; set; }
 
-
-        public AdminModel(IUserService userService)
+        public AdminModel(IUserService userService, IDoorService doorService)
         {
             _userService = userService;
+            _doorService = doorService;
         }
         public void OnGet()
         {
             Users = _userService.GetAllUsers();
+            Doors = _doorService.GetAllDoors().ToList();
         }
 
         [HttpPost]
@@ -46,6 +49,30 @@ namespace GarageDoorsWeb.Pages
 
             // Redirect to a different page or return a success message
             return RedirectToPage("/Admin");
+        }
+        public IActionResult OnPostRenameDoor(int id, string newName)
+        {
+            if (!string.IsNullOrWhiteSpace(newName))
+            {
+                var door = _doorService.GetDoorById(id);
+                if (door != null)
+                {
+                    door.DoorName = newName;
+                    _doorService.UpdateDoor(door);
+                }
+                return RedirectToPage();
+            }
+            else
+            {
+                // Handle the error case or set an error message if the newName is invalid
+                ModelState.AddModelError("", "The new door name cannot be empty.");
+                return Page();
+            }
+        }
+        public IActionResult OnPostRemoveDoor(int id)
+        {
+            _doorService.DeleteDoor(id);
+            return RedirectToPage();
         }
     }
 }
